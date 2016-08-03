0bf440cd06a95b4d2a64ed13ee74594192bdcf3d
AndriusK <andrius.k@sanitex.eu>
2016-08-03 18:38:50 +0300

    System.Web.Mvc, Version=5.5.0.0 rebuild

--- gen/Program.cs
+++ gen/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace RazorGenerator.MsBuild
 {
@@ -10,7 +11,15 @@ namespace RazorGenerator.MsBuild
     {
         static void Main(string[] args)
         {
-            var dir = args.Length > 1 ? args[0] : Environment.CurrentDirectory;
+            string arg1 = args.Length >= 1 ? args[0] : null;
+            if (arg1 == "-debug")
+            {
+                arg1 = null;
+                Debugger.Launch();
+                Debugger.Break();
+            }
+
+            var dir = arg1 ?? Environment.CurrentDirectory;
 			
             System.IO.Directory.SetCurrentDirectory(dir);
             var task = Razor.Start(dir);
